c43ee1f78e2c6e096086a6f334bf232884e5fb86
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-04-23 23:09:02 +0900

    水量と時間の入力フィールドを追加

--- coffee/pages/index.tsx
+++ coffee/pages/index.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import type { NextPage } from 'next';
 import { Params } from 'next/dist/server/router';
 
+import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import RefreshIcon from '@mui/icons-material/Refresh';
+import IconButton from '@mui/material/IconButton';
 import PauseIcon from '@mui/icons-material/Pause';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import RemoveIcon from '@mui/icons-material/Remove';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import TextField from '@mui/material/TextField';
 
 import useInterval from 'hooks/useInterval';
 
@@ -32,7 +36,9 @@ const displayTime = (time: number): string =>  {
 
 const Home: NextPage = ({ onUpdate }: Params) => {
 
+  const [fields, setFields] = React.useState<number>(1);
   const [timer, setTimer] = React.useState<number>(0);
+
   const [state, { start, stop }] = useInterval({
     interval: 10,
     onUpdate: () => timer < 3599990 ? setTimer(timer + 10): setTimer(3599990),  // 59:59:99
@@ -43,19 +49,40 @@ const Home: NextPage = ({ onUpdate }: Params) => {
 
       <p>{displayTime(timer)}</p>
 
-      <Button variant="contained">
-        <RefreshIcon  color="primary" onClick={() => {stop(); setTimer(0);}} />
-      </Button>
+      <div>
+        <Button variant="contained">
+          <RestartAltIcon  color="primary" onClick={() => {stop(); setTimer(0);}} />
+        </Button>
 
-      {state === 'RUNNING' ? (
-        <Button variant="contained">
-          <PauseIcon  color="primary" onClick={stop} />
-        </Button>
-      ) : (
-        <Button variant="contained">
-          <PlayArrowIcon color="primary" onClick={start} />
-        </Button>
-      )}
+        {state === 'RUNNING' ? (
+          <Button variant="contained">
+            <PauseIcon  color="primary" onClick={stop} />
+          </Button>
+        ) : (
+          <Button variant="contained">
+            <PlayArrowIcon color="primary" onClick={start} />
+          </Button>
+        )}
+      </div>
+
+      {[...Array(fields)].map((e: void[], i: number) => (
+        <div>
+          <TextField label="Water" required />
+          <TextField label="Time" />
+          {i === fields - 1 && (
+            <div>
+              <IconButton onClick={() => setFields(fields + 1)}>
+                <AddIcon />
+              </IconButton>
+              {fields > 1 && (
+                <IconButton onClick={() => setFields(fields - 1)}>
+                  <RemoveIcon />
+                </IconButton>
+              )}
+            </div>
+          )}
+        </div>
+      ))}
 
     </>
   )
