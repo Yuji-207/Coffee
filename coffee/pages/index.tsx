10a780bf66eb7664c9805a4cb395906c512240f5
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-04-23 21:33:45 +0900

    ストップウォッチの表示を変更

--- coffee/pages/index.tsx
+++ coffee/pages/index.tsx
@@ -3,8 +3,6 @@ import type { NextPage } from 'next';
 import { Params } from 'next/dist/server/router';
 
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import IconButton from '@mui/material/IconButton';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import PauseIcon from '@mui/icons-material/Pause';
@@ -12,18 +10,38 @@ import PauseIcon from '@mui/icons-material/Pause';
 import useInterval from 'hooks/useInterval';
 
 
+const displayTime = (time: number): string =>  {
+
+  let centi: number | string = time % 1000;
+  time -= centi;
+  centi /= 10;
+  centi = String(centi).padStart(2, '0');
+
+  let seconds: number | string = time % 60000;
+  time -= seconds;
+  seconds /= 1000;
+  seconds = String(seconds).padStart(2, '0');
+
+  let minutes: number | string = time / 60000;
+  minutes = String(minutes).padStart(2, '0');
+
+  return `${minutes}:${seconds}:${centi}`;
+
+}
+
+
 const Home: NextPage = ({ onUpdate }: Params) => {
 
   const [timer, setTimer] = React.useState<number>(0);
   const [state, { start, stop }] = useInterval({
     interval: 10,
-    onUpdate: () => setTimer(timer + 1),
+    onUpdate: () => timer < 3599990 ? setTimer(timer + 10): setTimer(3599990),  // 59:59:99
   })
 
   return (
     <>
 
-      <p>{timer}</p>
+      <p>{displayTime(timer)}</p>
 
       <Button variant="contained">
         <RefreshIcon  color="primary" onClick={() => {stop(); setTimer(0);}} />
