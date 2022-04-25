343ca3e3a0c69fe9f21eb65e85b07c0a4ebb89b2
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-04-25 19:43:09 +0900

    スタイルシートを_documentで定義

--- coffee/pages/_app.tsx
+++ coffee/pages/_app.tsx
@@ -8,14 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Coffee</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
       </Head>
       <Component {...pageProps} />
     </>
